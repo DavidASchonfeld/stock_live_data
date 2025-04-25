0533b8d85da21005a266e0b1a7d95bae89883348
David Schonfeld <davidaschonfeld@gmail.com>
2025-04-25 13:35:33 -0400

    Starting to create a script to get information from Open Weather Map API

---
+++ api_urls.py
@@ -0,0 +1,21 @@
+
+# API Url Information
+
+
+
+open_weather = {
+    "base_url_geo" : "http://api.openweathermap.org/",
+    "base_url" : "https://api.openweathermap.org/",
+    "current_weather" : {
+        "base" : "data/3.0/onecall",
+        "params" : {
+            "lat" : 0, #f"{lat}",
+            "lon" : 0, #f"{lon}"
+            # lat=33.44&lon=-94.04&appid={API key}
+        }
+    }
+}
+
+
+
+https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key}
---
+++ main.py
@@ -0,0 +1,53 @@
+
+# Python Libraries
+import os
+from typing import Any, Dict
+
+import requests
+import json
+
+from urllib.parse import urlencode, urljoin
+import copy
+
+import pandas as pd
+from pandas import DataFrame
+
+
+
+# My Files
+from api_key import open_weather
+from api_urls import open_weather
+
+
+# http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+
+def sendRequest(endStringRequest : str) -> Dict[Any, Any]:
+    
+
+    params = copy.deepcopy(open_weather["current_weather"]["params"])
+    params["lat"] = 33.44
+    params["lon"] = -94.04
+
+    params_formatted : str = urlencode(params)
+
+    # Merge End String
+    endUrlRequest :str = os.path.join(open_weather["current_weather"]["base"], params_formatted)
+
+    requestToSend_full : str = urljoin(open_weather["base_url"], endUrlRequest)
+
+    
+    try:
+        response : requests.Response = requests.get(requestToSend_full)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print("response.status_code: "+str(response.status_code))
+        raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
+    except Exception as error:
+        pass
+
+    responseContent_dict : dict = json.loads(response.content)
+
+    return responseContent_dict
+
+sendRequest("Test. This string's value is not yet implemented")
