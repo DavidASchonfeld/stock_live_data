e384002cf50327ddf3279f01dd5582ead97b1ab0
David Schonfeld <davidaschonfeld@gmail.com>
2025-05-05 14:13:21 -0400

    Running a Flask app in a Docker container on my Amazon Instance

--- flask_main.py
+++ flask_main.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes # type: ignore
+# from app import routes # type: ignore
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/hello')
+def hello():
+    return "Hello!"
+
+# Runs if you call the script directly
+# Does not run when you use Gunicorn to run this script
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
